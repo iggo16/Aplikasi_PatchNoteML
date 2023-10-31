a78f17e45c109195ccacf2b85ed1820451e99b64
wahid iggo <iggowahid1660@gmail.com>
2023-10-31 08:14:17 +0700

    first commit

---
+++ PatchMLBB/App.js
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import { Home } from './src/screens';
+export default function App() {
+  return <Home />;
+}
---
+++ PatchMLBB/src/screens/index.js
@@ -0,0 +1,6 @@
+
+import Home from "./Home";
+import Tier from "./Tier";
+import Patch from "./Patch";
+import Profile from "./Profile";
+export { Home, Tier, Patch, Profile }
